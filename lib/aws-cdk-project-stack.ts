203daaea3093551bab5f329b453b7f21a3eae92f
Afif Patel <afif.patel@aletha.ca>
2024-05-23 14:07:32 -0400

    stack setup

--- lib/aws-cdk-project-stack.ts
+++ lib/aws-cdk-project-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { IQueue, Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  
+    // L1 and L2 Construct of an S3 Bucket
+    const level1S3Bucket = new CfnBucket(this, 'Level1ConstructBucket', {
+      versioningConfiguration: {
+        status: "Enabled"
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const level2S3Bucket = new Bucket(this, 'Level2ConstructBucket', {
+      bucketName: "l2-bucket-aletha",
+      versioned: true
+    });
+
+    const queue: IQueue = new Queue(this, 'MyQueue', {
+      queueName: 'l2-bucket-update-message-queue'
+    });
+
+    // Add event notification for S3 bucket to send messages to SQS queue when a new object is created
+    level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED, new SqsDestination(queue));
   }
 }
