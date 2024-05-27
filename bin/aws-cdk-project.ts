b9b1d9e20017bdeeeac91890866329105fbf57d8
Afif Patel <afif.patel@aletha.ca>
2024-05-27 15:56:00 -0400

    cleanup

--- bin/aws-cdk-project.ts
+++ bin/aws-cdk-project.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkParentStack } from '../lib/aws-cdk-parent-stack';
 import { AwsCdkCodepipelineStack } from '../lib/aws-cdk-project-codepipeline-stack';
 import environmentConfig from './stack-config';
 
 const app = new cdk.App();
-// new CdkParentStack(app, 'CdkParentStack');
 new AwsCdkCodepipelineStack(app, 'AwsCdkCodepipelineStack', environmentConfig);
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export const handler = async(event: { name: string }) => {
-    const result: string = event.name ? `Nice work ${event.name}!` : 'Nice work!';
-    return result;
-}
