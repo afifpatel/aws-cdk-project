ec39a56f4b760ba5d23ba807a1dd1a8c5a39971a
Afif Patel <afif.patel@aletha.ca>
2024-05-27 16:08:33 -0400

    lambda changes

--- lib/aws-cdk-project-stack.ts
+++ lib/aws-cdk-project-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Bucket, CfnBucket, EventType } from 'aws-cdk-lib/aws-s3';
 import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
-import { IQueue, Queue } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
@@ -10,30 +9,24 @@ export class AwsCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-  
-    // L1 and L2 Construct of an S3 Bucket
-    const level1S3Bucket = new CfnBucket(this, 'Level1ConstructBucket', {
-      versioningConfiguration: {
-        status: "Enabled"
-      }
+    const bucketName = new cdk.CfnParameter(this, 'bucketName', {
+      type: 'String',
+      default: '',
+      description: 'S3 bucket name where Codepipeline will store lambda code',
     });
 
-    const level2S3Bucket = new Bucket(this, 'Level2ConstructBucket', {
-      bucketName: "l2-bucket-aletha",
-      versioned: true
+    const bucketKey = new cdk.CfnParameter(this, 'bucketKey', {
+      type: 'String',
+      default: '',
+      description: 'S3 bucket key which Codepipeline will use store lambda code',
     });
 
-    const queue: IQueue = new Queue(this, 'MyQueue', {
-      queueName: 'l2-bucket-update-message-queue'
-    });
-
-    // Add event notification for S3 bucket to send messages to SQS queue when a new object is created
-    level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED, new SqsDestination(queue));
+    const bucket = Bucket.fromBucketName(this, 'pipeline-bucket', bucketName.valueAsString);
 
     // Lambda function to be invoked by S3 bucket event notification
     new lambda.Function(this, 'LambdaFunction', {
-      functionName: 'l2-bucket-update-function',
-      code: new lambda.AssetCode('src'),
+      functionName: 'aws-cdk-pipeline-lambda',
+      code: lambda.Code.fromBucket(bucket, bucketKey.valueAsString),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_16_X,
       memorySize: 128,
